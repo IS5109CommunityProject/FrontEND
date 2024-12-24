8fb7bd15123309e83fee9f5f35bf4c6fae4f10b8
nipuna_akalana <hnethmin13@gmail.com>
2024-12-24 16:33:12 +0530

    style changes

--- medical_frontend/src/components/specific/HeroSection.jsx
+++ medical_frontend/src/components/specific/HeroSection.jsx
@@ -1,13 +1,16 @@
+import logo from '../../assets/images/logo-1.png';
+
 const HeroSection = () => {
   return (
     <div className="flex items-center gap-4 mb-8">
-    <img 
-      src="/placeholder.svg?height=60&width=60" 
-      alt="Faculty Logo" 
-      className="w-[60px] h-[60px]"
-    />
-    <h2 className="text-xl">Faculty of Computing</h2>
-  </div>  )
-}
+      <img 
+        src={logo} 
+        alt="Faculty Logo" 
+        className="w-[60px] h-[60px]"
+      />
+      <h2 className="text-xl">Faculty of Computing</h2>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
