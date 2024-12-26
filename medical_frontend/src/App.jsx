409d9f4aeb7255e62d25b72702573add22aaffdf
ShakishnaviM <shakishnavi27@gmail.com>
2024-12-26 22:19:22 +0530

    add lecture medical apply page

--- medical_frontend/src/App.jsx
+++ medical_frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import ExamMedical from "./Pages/ExamMedical";
 import Guidelines from "./Pages/Guidelines";
+import LectureMedical from "./Pages/LectureMedical";
 const App = () => (
     <Router>
         <Routes>
             <Route path="/guidelines" element={<Guidelines />} />
             <Route path="/exams" element={<ExamMedical />} />
             <Route path ="/" element={<Home/>}/>
-
+            <Route path="/lectureMedi" element={<LectureMedical/>}/>
 
         </Routes>
     </Router>
