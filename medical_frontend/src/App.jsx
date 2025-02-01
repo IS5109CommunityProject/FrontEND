3b0368873a9da02c2648105dad01b41415218d95
AnujikaSasindi <anujikasasindia@gmail.com>
2025-02-01 22:15:43 +0530

    Home page updated

--- medical_frontend/src/App.jsx
+++ medical_frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
 import ExamMedical from "./Pages/ExamMedical";
 import Guidelines from "./Pages/Guidelines";
 import LectureMedical from "./Pages/LectureMedical";
 import UserLogin from "./Pages/UserLogin";
 import Udashboard from "./Pages/udashboard";
 import MedicalFormResponse from "./Pages/response";
+import Home from "./Pages/Home";
 
 const App = () => (
     <Router>
         <Routes>
             <Route path="/lectureMedi" element={<LectureMedical/>}/>
-            <Route path ="/" element={<Home />}/>
             <Route path="/guidelines" element={<Guidelines />} />
             <Route path="/exams" element={<ExamMedical />} />
             <Route path="/user-login" element={<UserLogin />} />
             <Route path="/udashboard" element={<Udashboard/>} />
             <Route path="/response" element={<MedicalFormResponse/>} />
+            <Route path="/" element={<Home/>} />
         </Routes>
     </Router>
 );
